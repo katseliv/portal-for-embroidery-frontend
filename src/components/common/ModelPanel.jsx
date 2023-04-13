ec645ff14f7f4bddeb48a1fdeeeef1e744a802bf
katseliv <katseliv@yandex.ru>
2023-04-14 00:31:54 +0300

    Added texture to model. Refactored code. Added rotation to model.

--- src/components/common/ModelPanel.jsx
+++ src/components/common/ModelPanel.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import abstract from "../../images/abstract.png";
 
 const size = {
     height: "500px",
@@ -9,6 +8,7 @@ const size = {
 }
 
 const ModelPanel = (props) => {
+    const image = props.files.find(file => file.extension === "jpeg" || file.extension === "png");
     return (
         <div className="container p-5 overflow-hidden">
             <h1 className="h4 mb-4 fw-normal text-center">Model Panel</h1><br/>
@@ -17,50 +17,25 @@ const ModelPanel = (props) => {
                     <a-assets>
                         <a-asset-item id="object" src="/models/t-shirt/Tshirt.obj"></a-asset-item>
                         <a-asset-item id="material" src="/models/t-shirt/Tshirt.mtl"></a-asset-item>
-                        <img id="my-image" src={abstract} alt=""/>
-                        {/*<img id="image" src={`data:image/${props.image.extension};base64,${props.image.base64StringFile}`} alt=""/>*/}
+                        <img id="my-image" src={`data:image/${image.extension};base64,${image.base64StringFile}`} alt=""/>
                     </a-assets>
-                    <a-entity id="camera" camera position="0 0 0"
-                              orbit-controls="target: #target;
+                    <a-camera orbit-controls="target: #target;
                               enableDamping: true;
                               dampingFactor: 0.125;
                               rotateSpeed: 0.25;
                               rotateToSpeed: 0.04;
                               rotateTo: 0.172 4.140 2.797;
-                              logPosition: true;
-                              ">
-                    </a-entity>
-
+                              logPosition: true;"
+                              distance="1" position="0 0 0"></a-camera>
                     <a-entity id="target" obj-model="obj: #object;"
-                              material="sphericalEnvMap: #my-image; src: #my-image"
+                              material="normalMap: #my-image; sphericalEnvMap: #my-image; src: #my-image"
                               position="0 3 -15" rotation="0 -90 0" scale="10 10 10"></a-entity>
-
-
-                    {/*<a-entity id="camera" camera position="0 0 5" orbit-controls="*/}
-                    {/*                                                autoRotate: false;*/}
-                    {/*                                                target: #target;*/}
-                    {/*                                                enableDamping: true;*/}
-                    {/*                                                dampingFactor: 0.25;*/}
-                    {/*                                                rotateSpeed:0.14;*/}
-                    {/*                                                minDistance:3;*/}
-                    {/*                                                maxDistance:15;"*/}
-                    {/*                                                mouse-cursor="">*/}
-                    {/*</a-entity>*/}
-
-                    {/*<a-entity*/}
-                    {/*    id="camera"*/}
-                    {/*    camera="fov: 80; zoom: 1;"*/}
-                    {/*    position="0 2 5"*/}
-                    {/*    orbit-controls="autoRotate: false; target: #target; enableDamping: true; dampingFactor: 0.125; rotateSpeed:0.25; minDistance:3; maxDistance:100;"*/}
-                    {/*    mouse-cursor=""*/}
-                    {/*></a-entity>*/}
-
-                    <a-light
-                        color="#000"
-                        intensity="0.5"
-                        position="-23.821 62.630 -140.463"
-                        type="directional"></a-light>
                 </a-scene>
+            </div>
+            <div className="container py-3 w-50">
+                <button className="btn btn-lg btn-outline-secondary w-100" onClick={props.deactivateModelMode}>
+                    Back
+                </button>
             </div>
         </div>
     );
--- src/components/posts/PostProfileInfo.jsx
+++ src/components/posts/PostProfileInfo.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Carousel from "../common/Carousel";
 import Preloader from "../common/Preloader";
 import CommentListContainer from "../comments/CommentListContainer";
 import {base64ToArrayBuffer, saveByteArray} from "../../utils/file-helpers";
+import ModelPanel from "../common/ModelPanel";
 
 const PostProfileInfo = (props) => {
+    const [modelMode, setModelMode] = useState(false);
+
     if (!props.profile) {
         return <Preloader/>;
     }
+
+    const activateModelMode = () => {
+        setModelMode(true);
+    };
+
+    const deactivateModelMode = () => {
+        setModelMode(false);
+    };
 
     const mappedFiles = props.profile.files.map(file =>
         <a key={file.id} className="link-success px-1" href="javascript: undefined;"
@@ -21,52 +32,65 @@ const PostProfileInfo = (props) => {
 
     return (
         <div>
-            <div className="container p-5 overflow-hidden">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-5 mb-3">
-                            <Carousel files={props.profile.files}/>
-                        </div>
-                        <div className="col-7">
-                            <h1 className="h4 mb-5 fw-normal text-center">{props.profile.designName}</h1>
-                            <h6 className="h5 mb-3 fw-normal text-right">{props.profile.description}</h6>
-                            <h6 className="h5 mb-3 fw-normal text-right">As with all forms of matter, a liquid
-                                is composed of particles—atoms and molecules—that move about in relation to each
-                                other. Each state is distinguished from the others by the behavior of its
-                                particles. The particles in a liquid are situated near each other but
-                                are not as close together as the particles in solids—nor as far apart as those
-                                in gases. Unlike the particles in solids, which are fixed in place, the
-                                particles in liquids can slide past each other, though they cannot move as
-                                freely as the particles in gases.
-                            </h6>
-                            <h6 className="h5 mb-3 fw-normal text-right">
-                                Author: {props.profile.designerFirstName + " " + props.profile.designerLastName}
-                            </h6>
-                            <h6 className="h5 mb-3 fw-normal text-right">
-                                Files: {mappedFiles}
-                            </h6>
-                            <h6 className="h5 mb-3 fw-normal text-right">
-                                Tags: {mappedTags}
-                            </h6>
-                            {props.isAuthenticated && (props.authorizedUserRole === "ADMIN" || props.authorizedUserRole === "DESIGNER")
-                                ? <div className="btn-group mt-2 w-100" role="group">
-                                    <button className="btn btn-lg btn-outline-success"
-                                            onClick={props.activateCreateTagsMode}>
-                                        Add Tags
-                                    </button>
-                                    <button className="btn btn-lg btn-outline-success" onClick={props.activateEditMode}>
-                                        Edit
-                                    </button>
+            {modelMode
+                ? <ModelPanel files={props.profile.files} deactivateModelMode={deactivateModelMode}/>
+                : <div>
+                    <div className="container p-5 overflow-hidden">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-5 mb-3">
+                                    <Carousel files={props.profile.files}/>
                                 </div>
-                                : <button className="btn btn-lg btn-outline-secondary w-100"
-                                          onClick={() => props.navigate("/designs")}>
-                                    Back
-                                </button>}
+                                <div className="col-7">
+                                    <h1 className="h4 mb-5 fw-normal text-center">{props.profile.designName}</h1>
+                                    <h6 className="h5 mb-3 fw-normal text-right">{props.profile.description}</h6>
+                                    <h6 className="h5 mb-3 fw-normal text-right">As with all forms of matter, a liquid
+                                        is composed of particles—atoms and molecules—that move about in relation to each
+                                        other. Each state is distinguished from the others by the behavior of its
+                                        particles. The particles in a liquid are situated near each other but
+                                        are not as close together as the particles in solids—nor as far apart as those
+                                        in gases. Unlike the particles in solids, which are fixed in place, the
+                                        particles in liquids can slide past each other, though they cannot move as
+                                        freely as the particles in gases.
+                                    </h6>
+                                    <h6 className="h5 mb-3 fw-normal text-right">
+                                        Author: {props.profile.designerFirstName + " " + props.profile.designerLastName}
+                                    </h6>
+                                    <h6 className="h5 mb-3 fw-normal text-right">
+                                        Files: {mappedFiles}
+                                    </h6>
+                                    <h6 className="h5 mb-3 fw-normal text-right">
+                                        Tags: {mappedTags}
+                                    </h6>
+                                    {props.isAuthenticated && (props.authorizedUserRole === "ADMIN" || props.authorizedUserRole === "DESIGNER")
+                                        ? <div className="btn-group mt-2 w-100" role="group">
+                                            <button className="btn btn-lg btn-outline-success" onClick={activateModelMode}>
+                                                Show Model
+                                            </button>
+                                            <button className="btn btn-lg btn-outline-success"
+                                                    onClick={props.activateCreateTagsMode}>
+                                                Add Tags
+                                            </button>
+                                            <button className="btn btn-lg btn-outline-success"
+                                                    onClick={props.activateEditMode}>
+                                                Edit
+                                            </button>
+                                        </div>
+                                        : <div className="btn-group mt-2 w-100" role="group">
+                                            <button className="btn btn-lg btn-outline-success" onClick={activateModelMode}>
+                                                Show Model
+                                            </button>
+                                            <button className="btn btn-lg btn-outline-secondary"
+                                                    onClick={() => props.navigate("/designs")}>
+                                                Back
+                                            </button>
+                                        </div>}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <CommentListContainer postId={props.profile.id} userId={props.authorizedUserId}/>
+                    <CommentListContainer postId={props.profile.id} userId={props.authorizedUserId}/>
+                </div>}
         </div>
     );
 }
